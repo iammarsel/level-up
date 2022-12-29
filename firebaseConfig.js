0a41768876cfe8ef48a97f9f874866d1c6a54f53
iammarsel <fetmarsel2002@gmail.com>
2022-12-29 13:58:35 -0500

    env variables working

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
+
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
+import Constants from 'expo-constants';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-A-5s5tRQAK4TZQJhrPS6idXhZMJLqV4",
-  authDomain: "levelup-394ca.firebaseapp.com",
-  projectId: "levelup-394ca",
-  storageBucket: "levelup-394ca.appspot.com",
-  messagingSenderId: "992696262049",
-  appId: "1:992696262049:web:185f34b13d73ddeb64bfe0"
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId
 };
 
 // Initialize Firebase
